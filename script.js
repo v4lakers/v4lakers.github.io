02df3fc144fea05ff46daf6e58a24da7540b0bf8
Vivek Sharath <vsharath@cs.uchicago.edu>
2020-09-12 15:33:17 -0700

    Updating Hyper Links

--- script.js
+++ script.js
@@ -1,33 +1,33 @@
 var modalInfo = {
   1: {
     title: "Project 1",
-    info: "...",
+    info: "A Deep Learning Project that uses Recurrent Neural Networks to produce artificial music based on Kanye West melodies.",
     link: "https://v4lakers.github.io/kanyeWestMusicGeneration/"
   },
   2: {
     title: "Project 2",
-    info: "...",
-    link: "#"
+    info: "Software that allows users to build machine learning models and visualize Kobe Bryant's shooting history.",
+    link: "https://github.com/v4lakers/mlkobe"
   },
   3: {
     title: "Project 3",
-    info: "...",
-    link: "#"
+    info: "A project that uses various Machine Learning Models to determine which features of a dating profile helps nail that first impression.",
+    link: "https://github.com/v4lakers/CS329E_team11"
   },
   4: {
     title: "Project 4",
-    info: "....",
-    link: "#"
+    info: "Paper that examines the idea of a single language (the matrix language) serving as the underlying base in language switching.",
+    link: "https://www.aclweb.org/anthology/W18-3208.pdf"
   },
   5: {
     title: "Project 5",
-    info: "...",
-    link: "#"
+    info: "Project that uses Machine Learning and Signal Processing Tools to distinguish between artificial and authentic voice clips.",
+    link: "https://github.com/v4lakers/Voice-Conversion-Detection"
   },
   6: {
     title: "Project 6",
-    info: "...",
-    link: "#"
+    info: "Dashboard that gives key insight and analytics of my hiking adventures. ",
+    link: "https://v4lakers.github.io/hike/"
   }
 };
 
